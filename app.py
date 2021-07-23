558203be7023334c6132be5cac2f39fda5da3463
MilanaKazaryan <milana.v.kazaryan@gmail.com>
2021-07-23 15:27:15 +0400

    added html

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return f'<h1 align="center"> Hello, World!!!</h1>'
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
