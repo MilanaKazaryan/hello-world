62282042d70b005bfc4aae3e87c7f21124b96119
MilanaKazaryan <milana.v.kazaryan@gmail.com>
2021-07-23 16:10:25 +0400

    added login.html and register.html

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
-app = Flask(__name__)
+from flask import Flask, render_template, url_for,redirect, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from forms import LoginForm, RegistrationForm
+app = Flask(__name__) 
+app.config['SECRET_KEY'] = 'secretkeyformypage'
 @app.route('/')
 @app.route('/home')
 def home():
@@ -8,3 +11,13 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/login')
+def log_in():
+    form = LoginForm()
+    return render_template('login.html', form=form)
+
+@app.route('/register')
+def sign_up():
+    form = RegistrationForm()
+    return render_template('register.html', form=form)
