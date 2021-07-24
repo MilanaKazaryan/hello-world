d64b382bb1fa4c6d4419eb32bca870b2aea6a4ed
MilanaKazaryan <milana.v.kazaryan@gmail.com>
2021-07-24 13:45:43 +0400

    connecting to heroku postgresql

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from forms import LoginForm, RegistrationForm, PostsForm
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_user, current_user, logout_user, login_required
 
+
 app = Flask(__name__) 
 app.config['SECRET_KEY'] = 'secretkeyformypage'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dlngyjtkrrwfou:e961be084250b557dd63002d7ef906dd9c6f97f9a3f1aaf2fb390671c7a593c0@ec2-52-50-171-4.eu-west-1.compute.amazonaws.com:5432/dd6ddfjpfule5l'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ojrownguwtacgj:2f2989420092dc965198ecce8c2b6ee11340666ebd3c501b2643920bcb4deacf@ec2-35-168-145-180.compute-1.amazonaws.com:5432/d9fplprsbelsfn'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
@@ -20,9 +21,9 @@ def load_user(user_id):
     return Users.query.get(int(user_id))
 class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50))
-    email = db.Column(db.String(20), unique=True)
-    password = db.Column(db.String(50), unique=True)
+    username = db.Column(db.String(200))
+    email = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(200), unique=True)
     post = db.relationship('Posts', backref='author', lazy=True)
 
 
@@ -31,14 +32,17 @@ class Posts(db.Model):
     text = db.Column(db.String(120))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
+
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 @app.route('/login', methods=['GET','POST'],)
 def log_in():
@@ -54,17 +58,23 @@ def log_in():
         flash('Invalid email or password!', 'danger')
     return render_template('login.html', form=form)
 
-@app.route('/register', methods=['GET','POST'])
+
+@app.route('/register', methods=['POST'])
 def sign_up():
     form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
-        new_user = Users(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('log_in'))    
+    hashed_password = generate_password_hash(form.password.data, method='sha256')
+    new_user = Users(username=form.username.data, email=form.email.data, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    flash('Your account has been created! You are now able to log in', 'success')
+    return redirect(url_for('log_in'))    
+   
+
+@app.route('/register', methods=['GET'])
+def sign_up_page(): 
+    form = RegistrationForm()
     return render_template('register.html', form=form)
+
 
 @app.route('/newposts', methods=['POST', 'GET'])
 @login_required
@@ -77,11 +87,13 @@ def new_posts():
         return redirect('/posts')
     return render_template('newposts.html', post=post, name=current_user.username)
 
+
 @app.route('/posts', methods=['GET', 'POST'])
 @login_required
 def post():
     posts = Posts.query.all()
     return render_template('posts.html', posts=posts)
+
 
 @app.route('/logout')
 @login_required
